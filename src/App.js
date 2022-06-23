9e04ef6394d60490a317b3395efdb01d37c830cf
vivify <vivify@vivifys-MacBook-Air.local>
2022-06-23 21:40:38 +0200

    Prvi zadatak

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, 
+  Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/customers">Customers</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+          <Route path="/customers">
+            <Customers />
+          </Route>
+          <Route path="/products">
+            <Products />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Customers() {
+  return <h2>Customers</h2>;
+}
+
+function Products() {
+  return <h2>Products</h2>;
 }
 
 export default App;
